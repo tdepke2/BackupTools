108d943098097b7eeb2f8e4044bae8daeaf019e0
Thomas Depke <tommydepke@gmail.com>
2021-01-14 02:49:17 -0700

    Initial commit.

---
+++ BackupTools/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Quick desc:
+// CLI backup tool ideal for backing up files to a flash drive. Target filename to include/exclude are stored in a config file.
+// Supports diff checking, manual backup, and automatic backups. Ideally should work on linux too, and use CMake and TDD.
+
+int main(int argc, char** argv) {
+    std::cout << "Test.\n";
+    std::cin.get();
+    
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Work in progress tools for local file backups. Planned features are manual/automatic backups specified from a file, file diff checking, and support for remote drives if possible.
